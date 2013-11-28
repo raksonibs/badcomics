8a148d6ed312d0014d7d0dd4db862115fba0a25d
Oskar Niburski <oskarniburski@gmail.com>
2013-11-28 02:10:08 -0500

    planning how it will work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,12 +3,12 @@ require 'nokogiri'
 
 class WelcomeController < ApplicationController
   def index
-  	@data=getdata
+  	#@data=getdata
   end
 
   def getdata
 		#xml=URI.parse("http://wx.toronto.ca/festevents.nsf/tpaview?readviewentries")
-		@doc=Nokogiri::HTML(open("http://wx.toronto.ca/festevents.nsf/tpaview?readviewentries"))
+		@doc=Nokogiri::HTML(open("http://wx.toronto.ca/festevents.nsf/tpaview?readviewentries")).xpath("//viewentry")
 
 	end
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,4 +1,11 @@
 
 
 module WelcomeHelper
+=begin
+@data.each do |val| %>
+	<%= val.css("text")[0] %><br>
+	<%= val.css("text")[7] %> <br>
+	<br>
+<% end %> 
+=end
 end
