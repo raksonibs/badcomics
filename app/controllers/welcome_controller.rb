fbe78cc5fa4ea7e14d32d20fd4ecf8748a59aa14
Oskar Niburski <oskarniburski@gmail.com>
2013-12-03 11:06:11 -0500

    City hall needs only today's stuff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,9 +6,13 @@ require 'nokogiri'
 #errors if they come and say that if doesnt load probaly, doesnt submit to user (pick somethin
 	#else. then we get notifcation of errors)
 #might be better to only get the certain data once they pick their moods and money?
+
+#want to make smart databased on location, price, event, category wiht multiple tags-need name price, location, categories
+#tags based on 12 options right now. Will have to match based on description or title
+#need to remove location
 class WelcomeController < ApplicationController
   def index
-  	@data=clubcrawlers
+  	#@data=cityhall
   end
 
 
@@ -24,8 +28,23 @@ class WelcomeController < ApplicationController
   end
 
   def cityhall
-  	Nokogiri::HTML(open("http://wx.toronto.ca/festevents.nsf/tpaview?readviewentries")).xpath("//viewentry")
-  	#need to categorize data by music event etc
+  	info={}
+  	data=Nokogiri::HTML(open("http://wx.toronto.ca/festevents.nsf/tpaview?readviewentries")).xpath("//viewentry")
+  	countend=data.size
+  	count=0
+  	#info[name]=date start, time start, date end, time end, price, address, category
+  	#should only get events with current date, not all dates
+  	data.each do |val|
+  		info[val.xpath("//entrydata[@name='EventName']")[count]]=[val.xpath("//entrydata[@name='DateBeginShow']")[count],
+  																  val.xpath("//entrydata[@name='TimeBegin']")[count], 
+  																  val.xpath("//entrydata[@name='DateEndShow']")[count], 
+  																  val.xpath("//entrydata[@name='TimeEnd']")[count],
+  																  val.xpath("//entrydata[@name='Admission']")[count],
+  																  val.xpath("//entrydata[@name='Location']")[count],
+  																  val.xpath("//entrydata[@name='CategoryList']")[count]] unless val==nil
+  		count+=1
+  	end
+  	info
   end
 
   def nowmagazine
@@ -40,7 +59,7 @@ class WelcomeController < ApplicationController
   end
 
   def eventbrite
-  	JSON.parse((open("https://www.eventbrite.com/json/event_search?app_key=GUBRP2USZMDRRVPPSF&city=Toronto&date=today&max=100")).read)
+  	JSON.parse((open("http://www.eventbrite.com/json/event_search?app_key=GUBRP2USZMDRRVPPSF&city=Toronto&date=today&max=100")).read)
   	#<% @data["events"][1..100].each do |event|%>
 	#<%= event["event"]["title"] %><br>
 #<% end %>
@@ -70,6 +89,7 @@ class WelcomeController < ApplicationController
   end
 
   def localfacebookevents
+  	#facebook api graph
   end
 
   def unqiuefinds
@@ -79,5 +99,10 @@ class WelcomeController < ApplicationController
   def getIP
   end
 
-  
+  def meetup
+  	#&lat=43.7&lon=79.4
+	JSON.parse(open("https://api.meetup.com/2/open_events?status=upcoming&radius=25.0&city=Toronto&format=json&lat=43.7&lon=79.4&sig_id=122316042&sig=abfea66017a637350ba99380aecfb8e48811436f&key=7b794c3657477db4e107a7e366f7b5f").read)  	 	
+  end
+
+
 end
