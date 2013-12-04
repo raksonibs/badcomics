0932346df2849290db92378db830297e5801a146
Johnny <john@thebiggainer.com>
2013-12-04 18:35:15 -0500

    Updated and refactored front end files with subtitle text animation

--- app/assets/javascripts/whattodo.js
+++ app/assets/javascripts/whattodo.js
@@ -6,33 +6,44 @@ $(document).ready(function() {
 	$(".topnav").delay(200).animate({top:"+=45", opacity: 1},1200);
 
 	// top question and bottom statement fade-in on load.
-	$(".question").hide().delay(200).fadeIn(1200);
-	$(".bottom").hide().delay(200).fadeIn(1200);
+	$(".question, .bottom").hide().delay(200).fadeIn(1200);
 
 	// Category boxes animate from left to right on load.
 	$(".categories").animate({ opacity: 0 }, 0);
-	$(".categories").delay(200).animate({left:"+=20", opacity: 1},1800);
+	$(".categories").animate({left:"+=20", opacity: 1},1800);
 
-	// This is where 6 new boxes need to be brought in ajax
+	// This is where 6 new boxes need to be brought in.
+	// Need to stop massive delay!!
 
-	$(".categories1").hide();
-	$(".categories1").animate({ opacity: 0 }, 0);
+	$(".categories1, .price, .question2, .question3").hide().animate({ opacity: 0 }, 0);
 
-	$(".price").hide();
-	$(".price").animate({ opacity: 0 }, 0);
+	var firstpick;
+	var secondpick;
+	var thirdpick;
+
+	// $(".categories").one('click', function() {
+	// 	var firstpick = $('#id').text();
+	// 	alert(firstpick)
+	// });
 
 	$(".categories").one('click', function() {
-  		$(".categories").fadeOut( "slow", function() {
-  			$(".categories1").show().animate({left:"+=20", opacity: 1},1800);
+		var firstpick = $(".categorybox").val();
+  		$(".categories, .question").fadeOut( function() {
+  			$(".categories1, .question2").show().animate({left:"+20", opacity: 1},1800);
   		});
   		
 	});
 
 	$(".categories1").one('click', function() {
-  		$(".categories1, .question").fadeOut( "slow", function() {
-  			$(".price").show().animate({left:"+=20", opacity: 1},1800);
+		var secondpick = $(".categorybox").val(); 
+  		$(".categories1, .question2").fadeOut( function() {
+  			$(".price, .question3").show().animate({left:"+20", opacity: 1},1800);
   		});
   		
 	});
-	
+
+	$(".price").one('click', function() {
+		var thirdpick = $(".categorybox").val();
+	});	
+
 });
