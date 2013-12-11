8a3d7bd8bc54f834ad86e9e3c9e790338144c9da
Oskar Niburski <oskarniburski@gmail.com>
2013-12-11 11:28:56 -0500

    Feeling part of algorithm rudimentary made. Want to eventually make own sentimental anaylsis for events. Working on Fbook api

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
+require 'active_support/core_ext/numeric/time'
 #attractions for touristy like snakes and latteees. unique finds section?
 #also want like facebook friend parties
 #also funny things like sleep? maybe shouldnt tell them what to do lol
@@ -9,7 +10,7 @@ require 'nokogiri'
 class WelcomeController < ApplicationController
 	
   def index
-  	#@data=Event.makeevents
+  	@data=Event.all
   end
 
   def activitymap(activity)
@@ -50,10 +51,45 @@ class WelcomeController < ApplicationController
   	end
   end
 
+  def feelmap(feeling)
+  	if feeling=="Happy"
+  		feelmap=[]#cannot include every event because just happy, give them based on category here
+  	elsif feeling=="Sad"
+  		feelmap=["Comedy"]
+  	elsif feeling=="Excited"
+  		feelmap=["Party", "Music"]
+  	elsif feeling=="Blah"
+  		feelmap=["Comedy"]#[((rand()*2)-1).ceil]
+  	elsif feeling=="Nerdy"
+  		feelmap=["Tech"]
+  	elsif feeling=="Normal"
+  		feelmap=["Music", "Hang Out"]#[((rand()*8)-1).ceil]
+  	elsif feeling=="Celebrating"
+  		feelmap=["Party"]#[((rand()*2)-1).ceil]
+  	elsif feeling=="Lonely"
+  		feelmap=["Hang Out"]
+  	elsif feeling=="Restless"
+  		feelmap=["Music"]
+  	elsif feeling=="Lazy"
+  		feelmap=["Hang Out"]
+  	elsif feeling=="Festive"
+  		feelmap=["Seasonal"]
+  	elsif feeling=="Wierd"
+  		feelmap=["Misc."]
+  	elsif feeling=="Fancy"
+  		feelmap=["Theatre"]
+  	else
+  		feelmap=[]
+  	end
+  end
+
   def algorthim
   	#/result/happy/art/20
   	udist=["43.6426, 79.3871"] #cannot hardcode location and time
   	feeling,activity,money=params[:choice1], params[:choice2], params[:choice3] #also params[geolocation]
+  	
+  	feelingmap=feelmap(feeling)
+
   	timenow=Time.parse("Fri December 6 2013 10:00 AM")
 	@data=[]
 	if money[/-/]
@@ -77,12 +113,12 @@ class WelcomeController < ApplicationController
 					e.save
 				end
 				if e.category.count("/")==0
-					if activity.include?(e.category)
+					if activity.include?(e.category) || feelingmap.include?(e.category) #need to penalize if come from feeling rather than category choice (doing right now by worht less)
 						@data << e
 					end
 				else
 					e.category.split("/").each do |cat|
-						if activity.include?(cat)
+						if activity.include?(cat) || feelingmap.include?(cat)
 							@data<<e unless @data.include?(e)
 						end
 					end
@@ -99,44 +135,45 @@ class WelcomeController < ApplicationController
   			format.html { redirect_to "/whattodo"}
   		
 	  	elsif params[:button]=="rank" || (params[:button]!="dist" && params[:button]!="price" && params[:button]!="pricebot" && params[:button]!="rankbot" && params[:button]!="distbot" && params[:button]!="all" && params[:button]!="try")
-		  	@result, @scores=result(@data,udist, activity)
+		  	@result, @scores=result(@data,udist, activity, "rank", feeling, feelingmap)
+	
 		  	keys=makekeys(@result)
-	 		@result= keys.flatten.uniq.size!=3 ? uniquekeys(@result,"rank", @data, udist, activity) : @result
+	 		@result= keys.flatten.uniq.size!=3 ? uniquekeys(@result,"rank", @data, udist, activity, feeling, feelingmap) : @result
 	 		
 	 		format.js{ render :action => "/algorthim.js.erb" }
 		
 		elsif params[:button]=="rankbot"
-			@result, @scores=result(@data,udist, activity)
+			@result, @scores=result(@data,udist, activity, "rank", feeling, feelingmap)
 			@result=reversebot(@scores)
 			format.js{ render :action => "/algorthim.js.erb" }
 
 		elsif params[:button]=="pricebot"
-			@result, @scores=result(@data,udist,activity, "price")
+			@result, @scores=result(@data,udist,activity, "price", feeling, feelingmap)
 			@result=reversebot(@scores)
 			format.js{ render :action => "/algorthim.js.erb" }
 			
 		elsif params[:button]=="distbot"
-			@result, @scores=result(@data,udist, activity, "dist")
+			@result, @scores=result(@data,udist, activity, "dist", feeling, feelingmap)
 			@result=reversebot(@scores)
 			format.js{ render :action => "/algorthim.js.erb" }
 
 		elsif params[:button]=="price"
 	 		#only prices under their choice
-	 		@result, @scores =result(@data,udist,activity, "price")
+	 		@result, @scores =result(@data,udist,activity, "price", feeling, feelingmap)
 	 		keys=makekeys(@result)
-	 		@result= keys.flatten.uniq.size!=3 ? uniquekeys(@result,params[:button], @data, udist, activity) : @result
+	 		@result= keys.flatten.uniq.size!=3 ? uniquekeys(@result,params[:button], @data, udist, activity, feeling, feelingmap) : @result
 	 		format.js{ render :action => "/algorthim.js.erb" }
 
 
 		elsif params[:button]=="dist"
-	 		@result, @scores =result(@data,udist,activity, "dist")
+	 		@result, @scores =result(@data,udist,activity, "dist", feeling, feelingmap)
 	 		keys=makekeys(@result)
-	 		@result= keys.flatten.uniq.size!=3 ? uniquekeys(@result,params[:button], @data, udist, activity) : @result
+	 		@result= keys.flatten.uniq.size!=3 ? uniquekeys(@result,params[:button], @data, udist, activity, feeling, feelingmap) : @result
 
 	 		format.js{ render :action => "/algorthim.js.erb" }
 	 	elsif params[:button]=="all"
 
-	 		@result, @scores=result(@data,udist, activity)
+	 		@result, @scores=result(@data,udist, activity, "rank", feeling, feelingmap)
 	 		@result=@scores.sort
 	 		format.js{ render :action => "/all.js.erb" }
 
@@ -162,7 +199,7 @@ def makekeys(result)
 	keys
 end
 
-def uniquekeys(result,button, data,udist, activity)
+def uniquekeys(result,button, data,udist, activity, feeling, feelingmap)
 
 	choice="price" if button[/price/]
 	choice="rank" if button[/rank/]
@@ -170,13 +207,13 @@ def uniquekeys(result,button, data,udist, activity)
 	keys=makekeys(result)
   	while keys.flatten.uniq.size!=3
   		#catches repititons
-  		result, scores=result(data, udist, activity, choice)
+  		result, scores=result(data, udist, activity, choice, feeling, feelingmap)
 		keys=makekeys(result)
 	end
 	result
 end
 
-def result(data, udist,activity, choice='rank')
+def result(data, udist,activity, choice='rank', feeling, feelingmap)
 
 	first={}
   	firstn=""
@@ -187,9 +224,9 @@ def result(data, udist,activity, choice='rank')
   	@scores={}
 
   	data.each_with_index do |val,i|
-  		score=score(calculateprice(val), calculatedistance(val,udist), calculatepurity(val, activity), calculatetime(val)) if choice=="rank"
-  		score=score(calculateprice(val,true), 0, 0, 0) if choice=="price"
-  		score=score(0, calculatedistance(val,udist, true), 0, 0) if choice=="dist"
+  		score=score(calculateprice(val), calculatedistance(val,udist), calculatepurity(val, activity), calculatetime(val), calculatefeeling(val,feeling, activity, udist, feelingmap)) if choice=="rank"
+  		score=score(calculateprice(val,true), 0, 0, 0,0) if choice=="price"
+  		score=score(0, calculatedistance(val,udist, true), 0, 0,0) if choice=="dist"
   		@scores[score]=val.name
 
   		if i==0
@@ -248,6 +285,95 @@ def result(data, udist,activity, choice='rank')
 	return @result, @scores	
 end
 
+
+def calculatefeeling(val, feeling, activity, udist, feelingmap)
+	#doesnt work because may not get hangout with friends and still lonely
+	#so should not just pick categories if match. categories selected on match
+	#and feeling, so all feeling categories get into selection even if not
+	#in the activity, and get a bonus
+	mult=0
+
+	if feeling=="Happy"
+		mult=1 - (0.05*rand())
+	elsif feeling=="Sad"
+		if activity.include?("Comedy")
+			mult=1 - (0.05*rand())
+		elsif activity.include?("Hang Out") || activity.include?("Comedy") || activity.include?("Charity")
+			mult=1-(0.25*rand())
+		end
+	elsif feeling=="Excited"
+		if activity.include?("Music")
+			mult=1 - (0.05*rand())
+		elsif activity.include?("Party") || activity.include?("Sport") || activity.include?("Music")
+			mult=1-(0.25*rand())
+		end
+	elsif feeling=="Blah"
+		if activity.include?("Cinema")
+			mult=1 - (0.05*rand())
+		elsif activity.include?("Comedy")
+			mult=1-(0.25*rand())
+		end
+	elsif feeling=="Normal" 
+		if !(activity.include?("Misc.") || activity.include?("Charity"))
+			mult=1 - (0.05*rand())
+		end
+	elsif feeling=="Nerdy" 
+		if activity.include?("Tech")
+			mult=1 - (0.05*rand())
+		elsif activity.include?("Reading") || activity.include?("Museum")
+			mult=mult=1-(0.25*rand())
+		end
+	elsif feeling=="Fancy"
+		mult=1 - (0.05*rand())
+		if val.price.to_i>=50
+			mult=1
+		elsif (activity.include?("Museum") || activity.include?("Gallery") || activity.include?("Theatre") || activity.include?("Reading") || activity.include?("Art"))
+			mult=1-(0.15*rand())
+		end
+	elsif feeling=="Celebrating" && 
+		#need facebook api
+		if activity.include?("Party")
+			mult=1 - (0.05*rand())
+		elsif (activity.include?("Seasonal") || activity.include?("Music"))
+			mult=mult=1-(0.25*rand())
+		end
+	elsif feeling=="Lonely" 
+		if (activity.include?("Hangout"))
+		#need facebook api
+			mult=1 - (0.05*rand())
+		elsif activity.include?("Hangout")
+			mult=1-(0.25*rand())
+		end
+	elsif feeling=="Restless"
+		tnow=Time.now
+		if Time.parse(Date.today + val.time) < 2.hours.from_now
+			mult=1
+		elsif activity.include?("Music") || activity.include?("Music")
+			mult=1-(0.25*rand())
+		end
+
+	elsif feeling=="Lazy"
+		if val.distance_to([43.6426, -79.3871]) < 0.5
+			mult=1
+		elsif activity.include?("Hang Out") || activity.include?("Hang Out")
+			mult=1-(0.25*rand())
+		end
+	elsif feeling=="Festive" 
+		if (activity.include?("Seasonal"))
+			mult=1 - (0.05*rand())
+		elsif activity.include?("Seasonal")
+			mult=1-(0.25*rand())
+		end
+	elsif feeling=="Wierd" 
+		if (activity.include?("Misc.") || activity.include?("Misc."))
+			mult=1 - (0.05*rand())
+		end
+	else
+		mult= 0.5
+	end
+	score=mult*20
+end
+
   def calculatetime(val)
   	mult=0
   	time=val.time
@@ -256,7 +382,7 @@ end
   	else
   		mult=1
   	end
-  	score =mult*((33.33-25)*2)
+  	score =mult*((33.33-29)*2)
   end
 
   def calculateprice(val, full=false)
@@ -291,7 +417,7 @@ end
   	if full
   		score=(mult*100)-(rand()*5)
   	else
-  		score=(mult*25)-(rand()*5)
+  		score=(mult*20)-(rand()*5)
   	end
   end
 
@@ -325,7 +451,7 @@ end
 	 if full
 	 	score=(mult*100)-rand()
 	 else
-  		score=(mult*25)-rand()
+  		score=(mult*23)-rand()
   	end
   end
 
@@ -347,12 +473,12 @@ end
   	else
   		mult=0.4
   	end
-  	score=mult*33.3333
+  	score=mult*29
 
   end
 
-  def score(val,val2,val3,val4)
-  	val+val2+val3+val4
+  def score(val,val2,val3,val4, val5)
+  	val+val2+val3+val4+val5
   end
 
 
