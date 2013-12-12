6adb0ba027d9a12d1f209470c454e04070dfb954
Johnny <john@thebiggainer.com>
2013-12-12 17:28:24 -0500

    Refactored Javascript to remove slowdown

--- app/assets/javascripts/whattodo.js
+++ app/assets/javascripts/whattodo.js
@@ -1,169 +1,125 @@
-$(window).load(function() {
-  	$(".topnav").animate({ opacity: 0 }, 0);
-  	$(".topnav").delay(200).animate({top:"+=45", opacity: 1},1500);
-    function start() {
-      $(".buttons").hide()
-      $(".categories1, .price, .output, .question2, .question3, .question4, .output1, #blackness").hide().animate({ opacity: 0 });
-      $(".question, .bottom").hide().delay(100).fadeIn(800);
-      $(".question, .bottom").hide().delay(100).fadeIn(800);
-      $(".categories").animate({ opacity: 0 }, 0);
-      $(".categories").animate({left:"+=20", opacity: 1},800);
-      $(".categories").show();
+var choice1,
+    choice2,
+    choice3;
 
-  	$(".categories").on('click', function(e) {
-     		$(".categories, .question").fadeOut( function() {
-             	$(".categories1, .question2").show().animate({left:"+15", opacity: 1},800);
-         	});
-         	//e.stopPropogation();
-     	});
-  
-    $(".back1").on('click', function(event){
-      $(".categories1, .question2").fadeOut( function() {
-              $(".categories, .question").show().animate({left:"+15", opacity: 1},800);
-          });
-    })
-  	$(".categories1").on('click', function(e) {
-    		$(".categories1, .question2").fadeOut( function() {
-    			$(".price, .question3").show().animate({left:"+15", opacity: 1}, 800);
-    		});
-    		//e.stopPropogation();
-  	});
-    $(".back2").on('click', function(event){
-      $(".price, .question3").fadeOut( function() {
-              $(".categories1, .question2").show().animate({left:"+15", opacity: 1},800);
-          });
-    })
-  	$(".categories1").on("click", function(e) {
-    		$(".categories1, .question2").fadeOut( function() {
-    			$(".price, .question3").show().animate({left:"+15", opacity: 1},500);
-    		});
-    		//e.stopPropogation();
-  	});
+$(function() {
 
-  	$(".price").on('click', function(e) {
-    		$(".price, .question3").fadeOut( function() {
-          $("#blackness").show().animate({left:"+15", opacity: 1}, 1000).delay(500)
-          var black=setTimeout(function() {
-            $(".output, .question4, .output1").delay(500).show().animate({left:"+15", opacity: 1}, 1000);
-          }, 1000)
-          $("#blackness").fadeOut();
-    			
-    		});
-    		//e.stopPropogation();
-  	});
-  
-  	$(".categorybox").on('click', function() {
-      choice1=$(this).text()
+	$(".topnav").animate({ opacity: 0 }, 0).delay(200).animate({top:"+=45", opacity: 1},1500);
+
+  $(".categories").on('click', function(e) {
+    $(".categories, .question").fadeOut( function() {
+        $(".categories1, .question2").show().animate({left:"+15", opacity: 1},800);
     });
+    //e.stopPropogation();
+  });
 
-    $(".categorybox1").on('click', function() {
-      choice2=$(this).text()
+  $(".back1").on('click', function(event){
+    $(".categories1, .question2").fadeOut( function() {
+      $(".categories, .question").show().animate({left:"+15", opacity: 1},800);
     });
-  //result/happy/art/20
-    $(".categorybox2").on('click', function() {
-      choice3=$(this).text()
-      var rank;
-      var dist=null;
-      var price=null;
-      var rankbot=null;
-      var distbot=null;
-      var pricebot=null;
-      var all=null;
+  });
+
+  $(".categories1").on('click', function(e) {
+    $(".categories1, .question2").fadeOut( function() {
+      $(".price, .question3")
+        .show()
+        .animate({
+          left: "+15",
+          opacity: 1}, 800);
+    });
+    //e.stopPropogation();
+  });
+
+  $(".back2").on('click', function(event){
+    $(".price, .question3").fadeOut( function() {
+      $(".categories1, .question2").show().animate({left:"+15", opacity: 1},800);
+    });
+  });
+
+  $(".price").on('click', function(e) {
+    $(".price, .question3").fadeOut( function() {
+      $("#blackness")
+        .show()
+        .animate({left:"+15", opacity: 1}, 800)
+        .delay(400);
+
+      var black = setTimeout(function() {
+        $(".output, .question4, .output1")
+          .delay(500)
+          .show()
+          .animate({left:"+15", opacity: 1}, 1000);
+      }, 1000);
+
+      $("#blackness").fadeOut();
+    });
+    //e.stopPropogation();
+  });
+
+  $(".categorybox").on('click', function() {
+    choice1 = $(this).text();
+  });
+
+  $(".categorybox1").on('click', function() {
+    choice2 = $(this).text();
+  });
+
+  $(".categorybox2").on('click', function() {
+    choice3 = $(this).text();
+
+    $.ajax({
+        url: "/result/" + choice1 + "/" + choice2 + "/" + choice3,
+        type: "GET",
+        dataType: "script",
+        data: { choice1: choice1,
+                choice2: choice2,
+                choice3: choice3 }
+    });
+  });
+
+  $("#all").on("click", function() {
+    $(".buttons").toggle();
+
+    if (all === null) {
       $.ajax({
-            url: "/result/"+choice1+"/"+choice2+"/"+choice3,
-            type: "GET",
-            dataType: "script",
-            data: { choice1: choice1,
-                    choice2: choice2,
-                    choice3: choice3 },
-            complete: function(result){
-              rank=result.responseText.substr(20,(result.responseText.length)-3)
-            } 
-          })
-      //toggle all results
-      $("#all").on("click", function() {
-        $(".buttons").toggle()
-        
-        if (all===null) {
-        $.ajax({
-            url: "/result/"+choice1+"/"+choice2+"/"+choice3,
-            type: "GET",
-            dataType: "script",
-            data: { choice1: choice1,
-                    choice2: choice2,
-                    choice3: choice3,
-                    button: "all" },
-            complete: function(result){
-              all=result.responseText.substr(20,(result.responseText.length)-15)
-          }
-        })
-      } else {
-        $(".alloutput").toggle()
-      }
+          url: "/result/" + choice1 + "/" + choice2 + "/" + choice3,
+          type: "GET",
+          dataType: "script",
+          data: { choice1: choice1,
+                  choice2: choice2,
+                  choice3: choice3,
+                  button: "all" }
+      });
+    } else {
+      $(".alloutput").toggle();
+    }
+  });
 
-      $("#price").on("click", function(){
-        $.ajax({
-            url: "/result/"+choice1+"/"+choice2+"/"+choice3,
-            type: "GET",
-            dataType: "script",
-            data: { choice1: choice1,
-                    choice2: choice2,
-                    choice3: choice3,
-                    button: "all",
-                    button2: "price" }
-      })
-    })
-    $("#dist").on("click", function(){
-        $.ajax({
-            url: "/result/"+choice1+"/"+choice2+"/"+choice3,
-            type: "GET",
-            dataType: "script",
-            data: { choice1: choice1,
-                    choice2: choice2,
-                    choice3: choice3,
-                    button: "all",
-                    button2: "dist" }
-      })
-    })
-    $("#ourrank").on("click", function(){
-        $.ajax({
-            url: "/result/"+choice1+"/"+choice2+"/"+choice3,
-            type: "GET",
-            dataType: "script",
-            data: { choice1: choice1,
-                    choice2: choice2,
-                    choice3: choice3,
-                    button: "all",
-                    button2: "ourrank" }
-      })
-    })
-    $("#time").on("click", function(){
-        $.ajax({
-            url: "/result/"+choice1+"/"+choice2+"/"+choice3,
-            type: "GET",
-            dataType: "script",
-            data: { choice1: choice1,
-                    choice2: choice2,
-                    choice3: choice3,
-                    button: "all",
-                    button2: "time" }
-      })
-    })
-  })
-  })
-}
-  start()
+  $('.sort-order').on('click', function() {
+    var self = $(this);
+
+    $.ajax({
+        url: "/result/" + choice1 + "/" + choice2 + "/" + choice3,
+        type: "GET",
+        dataType: "script",
+        data: { choice1: choice1,
+                choice2: choice2,
+                choice3: choice3,
+                button: "all",
+                button2: self.data('sort-order') }
+    });
+  });
+
   $("#try").on("click", function() {
-    var rank;
-    var dist=null;
-    var price=null;
-    var rankbot=null;
-    var distbot=null;
-    var pricebot=null;
-    var all=null;
-    start()
-  })
-})
+      $(".question4, .alloutput").hide();
+      $('html, body').animate({scrollTop: '0px'}, 900);
+      start();
+  });
 
+  start();
+});
 
-
+function start() {
+  $(".categories, .categories1, .price, .output, .question2, .question3, .question4, .output1, #blackness").animate({ opacity: 0 }, 0);
+  $(".question, .bottom").fadeIn(500);
+  $(".categories").fadeIn().animate({left:"+20", opacity: 1}, 800);
+}
