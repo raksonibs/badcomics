21c38c33610ab0e8f58a650c7dcc57aca8731942
Oskar Niburski <oskarniburski@gmail.com>
2013-12-20 15:00:01 -0500

    Links to index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,6 +14,7 @@ class WelcomeController < ApplicationController
   		pricechoice=pricecount(@result)
   		feelingchoice=feelingscount(@result)[1]
   		@result=algorthim(feelingchoice, catchoice,pricechoice,true)
+
       @recent, @installedfriends=getrecent()
       @picture=FbGraph::User.fetch(current_user.uid).picture
       @events=soonevents
