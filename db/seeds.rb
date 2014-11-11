990c722c288fd5663dfd70b6a03a4a5fb235ab63
Oskar Niburski <oskarniburski@gmail.com>
2014-11-11 01:56:41 -0500

    Seedfile approach change

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,11 @@
+puts 'Destroying all Events'
 Event.destroy_all
 
 contents = File.read('eventsseedfile.txt')
-eventAll = []
-contents = contents[1..-2]
-eventSeed = Array(contents).flatten
-count = 0 
-puts 'Creating Events from Seedfile'
-contents.split(/\{/).each_with_index do |event, index|
-  symbolData = event.split('", ')
-  tmpEvent = Event.new()
-  symbolData.each do |attribute|
-    syms = attribute.split('=>')
-    syms[1] = Array(syms[1]) if syms[0] == ':categoryList'
-    syms[0] = ':url' if syms[0] == ':eventUrl'
-    tmpEvent.update_attributes({
-      syms[0].gsub(':', '') => syms[1]
-    })
-  end
-  tmpEvent.save!
+if contents == ""
+  puts 'Creating Events from Seedfile'
+  Event.createEvents
 end
+
+puts "Created #{Event.all.count} Events"
+ 
