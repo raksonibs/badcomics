aa4e51b163a34d85e7c5c1a793041e47237ddbb6
Oskar Niburski <oskarniburski@gmail.com>
2015-04-07 16:58:16 -0400

    More deleted. Hopefully nginx communicates with unicorn now

--- app/controllers/events_controller.rb
+++
@@ -1,8 +0,0 @@
-class EventsController < ApplicationController
-	def show
-		@event=Event.find_by_id(params[:event_id])
-		respond_to do |format|
-			format.js{ render :action => "event.js.erb" }
-		end
-	end
-end
