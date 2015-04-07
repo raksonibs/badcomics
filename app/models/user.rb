2349a62205b71381b3cefb3848c4b2b71d15d65a
Oskar Niburski <oskarniburski@gmail.com>
2015-04-07 13:35:43 -0400

    Movement

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,30 @@ class User < ActiveRecord::Base
   def self.adminUser
     User.find_by_email('thisbetterbeacompliment@badcomics.ca')
   end
+
+  def self.allImages
+    User.adminUser.images
+  end
+
+  def self.latestImage
+    User.adminUser.images.last 
+  end
+
+  def self.firstImage
+    User.adminUser.images.first
+  end
+
+  def self.nextImage(image)
+    imageArray = User.adminUser.images
+    thisIndex = (imageArray.map.with_index.to_a).to_h[image]    
+    return image if thisIndex >= imageArray.length - 1     
+    nextImage = imageArray[thisIndex + 1] 
+  end
+
+  def self.prevImage(image)
+    imageArray = User.adminUser.images
+    thisIndex = (imageArray.map.with_index.to_a).to_h[image]    
+    return image if thisIndex == 0    
+    nextImage = imageArray[thisIndex - 1] 
+  end
 end
