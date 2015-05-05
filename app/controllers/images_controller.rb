1898cebf655a6c80ae3108bd514955530e55d2ba
Oskar Niburski <oskarniburski@gmail.com>
2015-05-05 00:23:45 -0400

    Can edit, delete images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,14 +1,33 @@
 class ImagesController < ApplicationController
+  before_filter :set_subscriber
+
   def edit
+    @image = Image.find(params[:id])
   end
 
   def update
     @image = Image.find(params[:id])
+    @image.comic = params[:image][:comic] unless params[:image][:comic].nil?
+
+    if @image.update(image_params)
+      render :edit
+    else
+      render :edit
+    end
   end
 
   def destroy
     @image = Image.find(params[:id])
     @image.destroy!
     redirect_to home_path(current_user)
+  end
+
+  private
+  def set_subscriber
+    @subscriber = Subscriber.new
+  end
+
+  def image_params
+    params.require(:image).permit(:title)
   end
 end
