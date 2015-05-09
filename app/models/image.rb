f2bd5bfadb087cae8f7555a650f3f873af434e4c
Oskar Niburski <oskarniburski@gmail.com>
2015-05-09 17:45:27 -0400

    Last stuff

--- app/models/image.rb
+++ app/models/image.rb
@@ -10,6 +10,14 @@ class Image < ActiveRecord::Base
 
   friendly_id :title
 
+  def is_first?
+    order == Image.all.where(published: true).order(:order).last.order
+  end
+
+  def is_last?
+    order == Image.all.where(published: true).order(:order).first.order
+  end
+
   def lastOrder
     Image.sort(:order).last.order
   end
