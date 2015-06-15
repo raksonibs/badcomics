07fce67b64c1b4daae7dcdcae27ca8b2624a8297
Oskar Niburski <oskarniburski@gmail.com>
2015-06-14 23:39:52 -0400

    Update of footer and header

--- db/schema.rb
+++ db/schema.rb
@@ -16,39 +16,39 @@ ActiveRecord::Schema.define(version: 20150508031423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "images", force: true do |t|
+  create_table "images", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "comic_file_name"
-    t.string   "comic_content_type"
+    t.string   "comic_file_name",    limit: 255
+    t.string   "comic_content_type", limit: 255
     t.integer  "comic_file_size"
     t.datetime "comic_updated_at"
     t.integer  "order"
-    t.boolean  "published",          default: false
-    t.string   "title"
-    t.boolean  "show_title",         default: false
-    t.boolean  "large_img",          default: false
+    t.boolean  "published",                      default: false
+    t.string   "title",              limit: 255
+    t.boolean  "show_title",                     default: false
+    t.boolean  "large_img",                      default: false
   end
 
-  create_table "subscribers", force: true do |t|
-    t.string   "email"
+  create_table "subscribers", force: :cascade do |t|
+    t.string   "email",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "subscribed", default: true
-    t.boolean  "intro_sent", default: true
+    t.boolean  "subscribed",             default: true
+    t.boolean  "intro_sent",             default: true
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email",            null: false
-    t.string   "crypted_password", null: false
-    t.string   "salt",             null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "email",            limit: 255, null: false
+    t.string   "crypted_password", limit: 255, null: false
+    t.string   "salt",             limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.string   "phone_number"
-    t.string   "country_code"
-    t.string   "authy_id"
+    t.string   "name",             limit: 255
+    t.string   "phone_number",     limit: 255
+    t.string   "country_code",     limit: 255
+    t.string   "authy_id",         limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
