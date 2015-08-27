6f77c0586406b6fe90a884b613ef2a98ec1a56bd
Oskar Niburski <oskarniburski@gmail.com>
2015-08-27 00:30:04 -0400

    Some https stuff maybe?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   # For ActionMailer emails
-  config.action_mailer.default_url_options = { host: "hockeystick.co", protocol: 'https' }
+  config.action_mailer.default_url_options = { host: "badcomics.ca", protocol: 'https' }
 
   config.assets.version = '1.0'
   config.serve_static_assets = true
@@ -78,7 +78,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
