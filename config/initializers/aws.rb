a95d0dca534437ed24032e774ebb1a64deddc6ac
Oskar Niburski <oskarniburski@gmail.com>
2015-10-11 18:39:49 -0400

    Change for aws

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -16,8 +16,8 @@ Paperclip::Attachment.default_options.merge!(
  
 #config/initializers/s3_direct_upload.rb
 S3DirectUpload.config do |c|
-  c.access_key_id     = Rails.configuration.aws[:access_key_id]
-  c.secret_access_key = Rails.configuration.aws[:secret_access_key]
-  c.bucket            = Rails.configuration.aws[:bucket]
+  c.access_key_id     = Figaro.env.aws_access_key_id
+  c.secret_access_key = Figaro.env.aws_secret_access_key
+  c.bucket            = Figaro.env.bucket
   c.region            = "s3"
 end
